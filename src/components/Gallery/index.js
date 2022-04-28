b7af5e2f400e23ab6d563c9570107030f6cd5a89
Erin Knapp <erin.knapp@gmail.com>
2022-04-28 16:54:49 -0400

    added gallery and photolist components and tests

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,58 +1,15 @@
-import React, { useState } from "react";
-import { capitalizeFirstLetter } from "../../utils/helpers";
+import React from 'react';
+import PhotoList from '../PhotoList';
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
-function Nav() {
-const [categories] = useState([
-  {
-    name: 'commercial',
-    description: 'Photos of grocery stores, food trucks, and other commercial projects',
-  },
-  { name: 'portraits', description: 'Portraits of people in my life' },
-  { name: 'food', description: 'Delicious delicacies' },
-  { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
-]);
-const [currentCategory, setCurrentCategory] = useState(categories[0]);
-
+function Gallery(props) {
+  const { currentCategory } = props;
   return (
-    <header className="flex-row px-1">
-      <h2>
-        <a data-testid="link" href="/">
-          <span role="img" aria-label="camera">
-            {" "}
-            📸
-          </span>{" "}
-          Oh Snap!
-        </a>
-      </h2>
-      <nav>
-        <ul className="flex-row">
-          <li className="mx-2">
-            <a
-              href="#about"
-            >
-              About me
-            </a>
-          </li>
-          <li>
-            <span>Contact</span>
-          </li>
-          {categories.map((category) => (
-            <li className={`mx-1 ${
-                currentCategory.name === category.name && 'navActive'
-                }`} key={category.name}>
-              <span
-                onClick={() => {
-                  setCurrentCategory(category)
-                }}
-              >
-                {capitalizeFirstLetter(category.name)}
-              </span>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
+    <section>
+      <h1 data-testid="h1tag">{capitalizeFirstLetter(currentCategory.name)}</h1>
+      <p>{currentCategory.description}</p>
+      <PhotoList category={currentCategory.name} />
+    </section>
   );
 }
-
-export default Nav;
+export default Gallery;
