7f0700adc0c27bd27772a80377577ec23ecefb33
Erin Knapp <erin.knapp@gmail.com>
2022-05-01 15:30:07 -0400

    fixed all merge conflicts

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -19,7 +19,6 @@ function Nav(props) {
       </h2>
       <nav>
         <ul className="flex-row">
-<<<<<<< HEAD
           <li className="mx-2">
             <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
               About me
@@ -27,20 +26,10 @@ function Nav(props) {
           </li>
           <li className={`mx-2 ${contactSelected && 'navActive'}`}>
             <span onClick={() => setContactSelected(true)}>Contact</span>
-=======
-          <li className={`mx-2 ${contactSelected && 'navActive'}`}>
-          <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
-              About me
-            </a>
-          </li>
-          <li className={"mx-2"}>
-          <span onClick={() => setContactSelected(true)}>Contact</span>
->>>>>>> 5f93d7e7ea934b15cdff0807dae05aca8cc19394
           </li>
           {categories.map((category) => (
             <li
               className={`mx-1 ${
-<<<<<<< HEAD
                 currentCategory.name === category.name && !contactSelected && 'navActive'
                 }`}
               key={category.name}
@@ -50,16 +39,6 @@ function Nav(props) {
                   setCurrentCategory(category);
                   setContactSelected(false);
                 }}
-=======
-              currentCategory.name === category.name && !contactSelected && `navActive`
-            }`}
-              key={category.name}
-            >
-              <span onClick={() => {
-                setCurrentCategory(category);
-                setContactSelected(false);
-              }}
->>>>>>> 5f93d7e7ea934b15cdff0807dae05aca8cc19394
               >
                 {capitalizeFirstLetter(category.name)}
               </span>
